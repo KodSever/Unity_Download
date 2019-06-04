c1b38e388619072114fb134dddba43ba7dea0844
Yiğit KARAKOYUN <38832410+KodSever@users.noreply.github.com>
2019-06-04 21:53:05 +0300

    Update image yükleme.cs

--- image yükleme.cs
+++ image yükleme.cs
@@ -10,3 +10,15 @@ void enumDosyadanYukle(string path = @"D:\fatman\texture.png")
     image.GetComponent<RawImage>().texture = texture;
   }
 }
+
+//Dosyayı Texture Olarak Yükle
+void enumDosyadanYukle(string path = @"D:\fatman\texture.png", GameObject image)
+{
+  Texture2D texture = new Texture2D(2, 2);
+  byte[] byteArray = File.ReadAllBytes(path);
+  bool tempYuklendi = texture.LoadImage(byteArray);
+  if (tempYuklendi)
+  {
+    image.GetComponent<RawImage>().texture = texture;
+  }
+}
